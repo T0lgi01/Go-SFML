d95a504d45f422c22810e5c233bb1ff968f307b8
T0lgi01 <1923288@uni-wuppertal.de>
2024-02-28 20:05:41 +0100

    Some tests. Explicit is best. BUG: ...& 3 +...

--- src/main3.cc
+++ src/main3.cc
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <vector>
+#include <iostream>
 
 enum cellstate{empty = 0, black = 1, white = 2, ko = 3}; // .#OX
 enum cellcolor{emptycolor = 0x0000000, whitecolor = 0xffffffff, blackcolor = 0x000000ff};
@@ -20,19 +21,17 @@ bool in_bounds(uint8_t x, uint8_t y){
 }
 
 void board_clean_flags(Board *B){ // All cells cast to .#OX
-    uint8_t j = 0;
-    for (uint8_t i = 0; i < 19; ++i){
-        for (; j < 19; ++j){
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
             B->board[i][j] &= 3;
         }
     }
 }
 
 void board_print(Board *B){
-    uint8_t j = 0;
-    for (uint8_t i = 0; i < 19; ++i){
-        for (j = 0; j < 19; ++j){
-            printf("%c%c", ".#OX*#OX"[B->board[i][j] & 3 + ((i%6 == 3 && j%6 == 3) << 2)], " \n"[j%19 == 18]);
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
+            printf("%c%c", ".#OX*#OX"[B->board[i][j] & 3 + ((i%6 == 3 && j%6 == 3) << 2)], " \n"[j == 18]);
         }
     }
     printf("Prisoners. B: %i, W: %i\nCurrent Move: %c", B->black_prisoners, B->white_prisoners, "BW"[B->current_color == white]);
@@ -40,9 +39,8 @@ void board_print(Board *B){
 
 void board_remove_group(Board *B, uint8_t x, uint8_t y){
     uint8_t *current_cell;
-    uint8_t j = 0;
-    for (uint8_t i = 0; i < 19; ++i){
-        for (; j < 19; ++j){
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
             current_cell = &B->board[i][j];
             if ((*current_cell & 4) != 0){
                 B->black_prisoners += (*current_cell & 3) == white;
@@ -107,7 +105,7 @@ bool board_play_move(Board *B, uint8_t x, uint8_t y){
         }
         board_clean_flags(B);
     }
-    printf("Played Move at %i, %i.\n", x, y);
+    //printf("Played Move at %i, %i.\n", x, y);
     B->current_color ^= 3;
     return true;
 }
@@ -120,16 +118,128 @@ void board_setup_game(Board *B){
     }
 }
 
+void __attribute__((noinline)) board_print1(Board *B){ // 5684 cycles on -O3
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
+            printf("%c%c", ".#OX*#OX"[(B->board[i][j] & 3) + ((i%6 == 3 && j%6 == 3) << 2)], " \n"[j == 18]);
+        }
+    }
+}
+
+void __attribute__((noinline)) board_print2(Board *B){ // 7876 cycles on -O3
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
+            std::cout << ".#OX*#OX"[(B->board[i][j] & 3) + ((i%6 == 3 && j%6 == 3) << 2)] << " \n"[j == 18];
+        }
+    }
+}
+
+void __attribute__((noinline)) board_print3(Board *B){ // 4112 cycles on -O3
+    uint8_t which;
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
+            which = B->board[i][j] & 3;
+            if (which == empty && i%6 == 3 && j%6 == 3){
+                printf("*");
+            }
+            else if (which == empty){
+                printf(".");
+            }
+            else if (which == black){
+                printf("#");
+            }
+            else if (which == white){
+                printf("O");
+            }
+            else {
+                printf("X");
+            }
+            if (j == 18){
+                printf("\n");
+            }
+            else {
+                printf(" ");
+            }
+        }
+    }
+}
+
+void __attribute__((noinline)) board_print4(Board *B){ // 5219 cycles on -O3
+    uint8_t which;
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
+            which = B->board[i][j] & 3;
+            if (which == empty && i%6 == 3 && j%6 == 3){
+                printf("*");
+            }
+            else if (which == empty){
+                printf(".");
+            }
+            else if (which == black){
+                printf("#");
+            }
+            else if (which == white){
+                printf("O");
+            }
+            else {
+                printf("X");
+            }
+            printf("%c", " \n"[j == 18]);
+        }
+    }
+}
+
+void __attribute__((noinline)) board_print5(Board *B){ // 4125 cycles on -O3
+    uint8_t which;
+    for (int i = 0; i < 19; ++i){
+        for (int j = 0; j < 19; ++j){
+            which = B->board[i][j] & 3;
+            if (which == empty){
+                if (i%6 == 3 && j%6 == 3){
+                    printf("*");
+                } else {
+                    printf(".");
+                }  
+            }
+            else if (which == black){
+                printf("#");
+            }
+            else if (which == white){
+                printf("O");
+            }
+            else {
+                printf("X");
+            }
+            if (j == 18){
+                printf("\n");
+            }
+            else {
+                printf(" ");
+            }
+        }
+    }
+}
+
 int main(){
     Board B1; 
     board_setup_game(&B1);
 
-    std::vector<std::vector<uint8_t>> moves = {{2, 0}}; 
+    int moves[][2] = {{2, 0}, {3, 0}, {1, 0}, {2, 1}, {13, 17}, {1, 1}, {0, 0}, {0, 1}};
 
     for (auto move : moves) {
         board_play_move(&B1, move[0], move[1]);
-        board_print(&B1);
-        printf("\n\n");
     }
 
-} //g++ -c src/main3.cc && g++ main3.o -o Go3 && ./Go3
+    board_print1(&B1);
+    board_print2(&B1);
+    board_print3(&B1);
+    board_print4(&B1);
+    board_print5(&B1);
+
+    return 0;
+} 
+// g++ -O3 -g -c src/main3.cc && g++ main3.o -o Go3 && ./Go3
+// rm ./callgrind*
+// valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./Go3
+// kcachegrind
+// g++ -O3 -g -c src/main3.cc && g++ main3.o -o Go3 && rm ./callgrind* && valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./Go3 && kcachegrind
